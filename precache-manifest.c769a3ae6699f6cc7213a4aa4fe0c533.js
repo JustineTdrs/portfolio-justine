20ebc3c94fdd58ed9d9ed65bf6cf0daa5aeb39aa
JustineTadros <71759215+JustineTadros@users.noreply.github.com>
2024-04-22 23:06:23 +0200

    Updates

---
+++ precache-manifest.c769a3ae6699f6cc7213a4aa4fe0c533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f5b723e7a4316b720ad89cb8d95ef2",
+    "url": "/git@github.com:JustineTdrs/Portfolio2024.git/index.html"
+  },
+  {
+    "revision": "3510ec840ec761d1eba3",
+    "url": "/git@github.com:JustineTdrs/Portfolio2024.git/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0dabf341fee51196b826",
+    "url": "/git@github.com:JustineTdrs/Portfolio2024.git/static/css/main.64388ba0.chunk.css"
+  },
+  {
+    "revision": "3510ec840ec761d1eba3",
+    "url": "/git@github.com:JustineTdrs/Portfolio2024.git/static/js/2.c2e1b725.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/git@github.com:JustineTdrs/Portfolio2024.git/static/js/2.c2e1b725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dabf341fee51196b826",
+    "url": "/git@github.com:JustineTdrs/Portfolio2024.git/static/js/main.15e42cbc.chunk.js"
+  },
+  {
+    "revision": "7515793b8d8d7ee22f93",
+    "url": "/git@github.com:JustineTdrs/Portfolio2024.git/static/js/runtime-main.07a274cd.js"
+  }
+]);
